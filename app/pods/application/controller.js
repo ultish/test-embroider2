a6337e612d1e1b0bb2a69972a8ae5854a470a494
Jimmy <ultish@gmail.com>
2021-10-16 18:48:28 +1100

    tailwind trial

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,3 +1,5 @@
 import Controller from '@ember/controller';
 
-export default class ApplicationController extends Controller {}
+export default class ApplicationController extends Controller {
+  someVariable = 'some variable in controller';
+}
---
+++ app/tailwind.config.js
@@ -0,0 +1,13 @@
+/*global module*/
+module.exports = {
+  // mode: process.env.NODE_ENV ? 'jit' : undefined,
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
